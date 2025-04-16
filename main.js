ec03bd15accad1113af1b8d9d321f85da75855a8
isaservienski <145451992+isaservienski@users.noreply.github.com>
2025-04-16 07:54:20 -0300

    at

--- main.js
+++ main.js
@@ -35,9 +35,9 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0) {
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+        return dias + " 200 " + horas + " 4.800 " + minutos + " 28800 " + segundos + " 1,7";
     } else {
-        return "Prazo Finalizado";
+        return "Prazo em andamento";
     }
 }
 
